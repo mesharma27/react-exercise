21d674e974b1afe9e31ab76dc344147ebdcf7346
megha <megha2701@gmail.com>
2024-10-04 12:04:02 +0800

    filtered emojis

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
+import styled from 'styled-components';
+import {Input} from 'semantic-ui-react';
+import allEmojis from "./resources/emojis.json";
+import { useState } from 'react';
+
 export const App = () => {
-  return <div>Hi there</div>;
+
+
+  const[filteredEmojis, setFilteredEmoji] = useState([]);
+
+/*  const filterEmojis =*/
+  const handleFilteredEmojis = (filterName) => {
+    const filtered =  allEmojis.filter((emoji) => {
+      return emoji.name.includes(filterName);
+    })
+    setFilteredEmoji(filtered);
+  }
+
+  return <AppContainer>
+    <Input
+      fluid
+    icon={"search"}
+    placeholder="Search"
+    onChange={(ab)=> {
+      handleFilteredEmojis(ab.target.value)
+    }}
+    />
+    {
+      filteredEmojis.map((emoji) => {
+      return (
+        <div>{emoji.name}{emoji.character}{emoji.code}</div>
+      )
+    })}
+  </AppContainer>;
 };
+const AppContainer = styled.div`
+margin: 100px 25%
+`
